90aae7b66639444d24ec89397b32b02cfca02ddb
Ruán <ruan@murgatroyd.me>
2023-09-21 00:12:18 +0100

    Move to Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<slot></slot>
+
+<footer>
+    <span><a href="https://rjm.ie/" class="rjm">RJM</a> - © 2023. Made with ❤️ (and Svelte).</span>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { LogIn } from "lucide-svelte";
+</script>
+
+<svelte:head>
+    <title>Sylgar</title>
+</svelte:head>
+
+<h1 class="title large">Sylgar</h1>
+<h2 class="subtitle medium">PDF Hosting</h2>
+
+<form method="POST">
+    <label for="password">
+        <span class="label">Password</span>
+    </label>
+    <input type="password" name="password" placeholder="password" id="password" />
+
+    <div class="form-element">
+        <button type="submit" class="">
+            <LogIn color="var(--green)" size="24" />
+            Login
+        </button>
+    </div>
+</form>
