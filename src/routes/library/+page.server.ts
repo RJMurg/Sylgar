ee9a8479c6d73bc1df631719a51799596a484bbe
Ruán <ruan@murgatroyd.me>
2023-09-21 17:02:02 +0100

    Some work on serving

--- src/routes/library/+page.server.ts
+++ src/routes/library/+page.server.ts
@@ -1,5 +1,9 @@
 import type { PageServerLoad } from './$types';
+import fs from 'fs';
 
 export const load = (async () => {
+    let filenames = fs.readdirSync('./static/library');
+
+    console.log(filenames);
     return {};
 }) satisfies PageServerLoad;
--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { base } from '$app/paths';
+
     import type { PageData } from './$types';
     
     export let data: PageData;
@@ -7,3 +9,7 @@
 <svelte:head>
     <title>Sylgar - Library</title>
 </svelte:head>
+
+<h1 class="title large">Library</h1>
+<h2 class="subtitle small">Select from a range of sourcebooks</h2>
+<a href="{base}/library/Zweihander.pdf">Test</a>
